156b8da310ead5d0ff8f050e71766786c4564642
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-06-07 09:33:47 +0100

    Inflate the Menu, set the textView to scroll vertically

--- app/src/main/java/com/andela/webservice/MainActivity.java
+++ app/src/main/java/com/andela/webservice/MainActivity.java
@@ -2,12 +2,42 @@ package com.andela.webservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = (TextView) findViewById(R.id.textView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+
+        for(int i = 0; i<100; i++) {
+            updateDisplay("Task " + i);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_do_task) {
+            updateDisplay("Task done!");
+        }
+        return true;
+    }
+
+    private void updateDisplay(String s) {
+        output.append(s + "\n");
     }
 }
