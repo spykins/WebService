a872997072fae44bc4924b6a3eaf951bd44c1725
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-06-11 11:18:49 +0100

    Create an Adapter class for the ListView

---
+++ app/src/main/java/com/andela/webservice/FlowerAdapter.java
@@ -0,0 +1,42 @@
+package com.andela.webservice;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.andela.webservice.model.Flower;
+
+import java.util.List;
+
+/**
+ * Created by Spykins on 11/06/16.
+ */
+public class FlowerAdapter extends ArrayAdapter<Flower> {
+    private Context context;
+    @SuppressWarnings("unused")
+    private List<Flower> flowerList;
+
+    public FlowerAdapter(Context context, int resource, List<Flower> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.flowerList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater =
+                (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_flower, parent, false);
+
+        //Display flower name in the TextView widget
+        Flower flower = flowerList.get(position);
+        TextView tv = (TextView) view.findViewById(R.id.textView1);
+        tv.setText(flower.getName());
+        return view;
+    }
+}
