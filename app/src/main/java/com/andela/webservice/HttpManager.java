25f7f80001315405aa39df54d7a6dac6282069e0
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-06-15 10:04:00 +0100

    Set up for sending param, clear files not needed

--- app/src/main/java/com/andela/webservice/HttpManager.java
+++ app/src/main/java/com/andela/webservice/HttpManager.java
@@ -18,118 +18,35 @@ public class HttpManager {
     public static String getData(String uri) {
 
         BufferedReader reader = null;
-        try{
-            URL url = new URL(uri);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            StringBuilder sb = new StringBuilder();
-            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String line;
-            while((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            return sb.toString();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*
-            //More on HTTPURLCONNECTION
-
-        HttpURLConnection connection = null;
-        String responseContent = null;
-        StringBuilder builder = new StringBuilder();
 
         try {
             URL url = new URL(uri);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            builder.append(connection.getResponseCode())
-                    .append(" ")
-                    .append(connection.getResponseMessage())
-                    .append("\n");
-
-            //This gets the header and prints the content of the Header.....
-            Map<String, List<String>> map = connection.getHeaderFields();
-            for (Map.Entry<String, List<String>> entry : map.entrySet())
-            {
-                if (entry.getKey() == null)
-                    continue;
-                builder.append( entry.getKey())
-                        .append(": ");
-
-                List<String> headerValues = entry.getValue();
-                Iterator<String> it = headerValues.iterator();
-                if (it.hasNext()) {
-                    builder.append(it.next());
-
-                    while (it.hasNext()) {
-                        builder.append(", ")
-                                .append(it.next());
-                    }
-                }
-
-                builder.append("\n");
-            }
-
-                   return connection.getInputStream().toString();
-            //This returns the
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(connection != null) {
-                connection.disconnect();
-            }
-        }
-
-        return builder.toString();
-        }
-        */
-        return null;
-    }
-
-    public static String getData(String uri, String userName, String password) {
-
-        BufferedReader reader = null;
-        byte[] loginByte = (userName + ":" + password).getBytes();
-        //Now we have a byte Array represent the credentials..
-        StringBuilder loginBuilder = new StringBuilder()
-                .append("Basic ")
-                .append(Base64.encodeToString(loginByte, Base64.DEFAULT));
-        //Now the loginBuilder contains the credential, encoded and ready to send to the webServer
-
-        try{
-            URL url = new URL(uri);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            /*
-                Before connecting with the connection Object, we need to set a request property
-                The header value... The header value will have a name of Authorisation and a value
-                created by the loginBuilder object
-             */
-
-            con.addRequestProperty("Authorization", loginBuilder.toString());
-            //Now as long as we connect with the correct userName and password...
-            // you should be able to connect and get data back
 
             StringBuilder sb = new StringBuilder();
             reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
             String line;
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 sb.append(line + "\n");
             }
+
             return sb.toString();
-        } catch (MalformedURLException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
         }
 
-        return null;
     }
 
 }
--- app/src/main/java/com/andela/webservice/MainActivity.java
+++ app/src/main/java/com/andela/webservice/MainActivity.java
@@ -1,13 +1,13 @@
 package com.andela.webservice;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,31 +16,28 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.andela.webservice.model.Flower;
-import com.andela.webservice.parser.FlowerJsonParser;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends ListActivity {
-    private TextView output;
-    private ProgressBar progressBar;
-    private List<MyTask> tasks;
-    private List<Flower> flowerList;
-    public static final String PHOTO_BASE_URL = "http://services.hanselandpetal.com/photos/";
+public class MainActivity extends Activity {
+
+    TextView output;
+    ProgressBar pb;
+    List<MyTask> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
-        progressBar.setVisibility(View.INVISIBLE);
+
+//		Initialize the TextView for vertical scrolling
+        output = (TextView) findViewById(R.id.textView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+
+        pb = (ProgressBar) findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
+
         tasks = new ArrayList<>();
-        //output = (TextView) findViewById(R.id.textView);
-        //output.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
@@ -51,107 +48,64 @@ public class MainActivity extends ListActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_do_task) {
-            if(isOnline()) {
-                requestData("http://services.hanselandpetal.com/secure/flowers.json");
+        if (item.getItemId() == R.id.action_do_task) {
+            if (isOnline()) {
+                requestData("http://services.hanselandpetal.com/feeds/flowers.json");
             } else {
                 Toast.makeText(this, "Network isn't available", Toast.LENGTH_LONG).show();
             }
-
         }
-        return true;
+        return false;
     }
 
     private void requestData(String uri) {
+
         MyTask task = new MyTask();
-        //This makes serial request
         task.execute(uri);
-        //For parrallel processing
-        //task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "param1", "param2", "param3");
     }
 
-    private void updateDisplay() {
-        /*if(flowerList != null) {
-            for (Flower flower : flowerList) {
-                output.append(flower.getName() + "\n");
-            }
-
-        }*/
-
-        FlowerAdapter adapter = new FlowerAdapter(this, R.layout.item_flower, flowerList);
-        setListAdapter(adapter);
+    protected void updateDisplay(String result) {
+        output.append(result + "\n");
     }
 
     protected boolean isOnline() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        if(netInfo != null && netInfo.isConnectedOrConnecting()) {
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
             return true;
         } else {
             return false;
         }
     }
 
-    private class MyTask extends AsyncTask<String, String, List<Flower>> {
-        //Async task now returns a List of Flower objects
+    private class MyTask extends AsyncTask<String, String, String> {
+
         @Override
         protected void onPreExecute() {
-            //updateDisplay("Starting task");
-            if(tasks.size() == 0) {
-                progressBar.setVisibility(View.VISIBLE);
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.VISIBLE);
             }
             tasks.add(this);
         }
 
-        /*
-            Doin the background parsing in the background thread
-         */
-
         @Override
-        protected List<Flower> doInBackground(String... params) {
-            String content = HttpManager.getData(params[0],"feeduser","feedpassword");
-            flowerList = FlowerJsonParser.parseFeed(content); //getting data from JSon feed
-            //Getting image for each flower name in the list
-            /*for(Flower flower : flowerList) {
-                String imageUrl = PHOTO_BASE_URL + flower.getPhoto();
-                try {
-                    InputStream in = (InputStream) new URL(imageUrl).getContent();
-                    //This retrieves the entire content of the location in one request
-                    //It comes back as a blub of data n we can access it through the input stream
-                    Bitmap bitmap = BitmapFactory.decodeStream(in);
-                    flower.setBitmap(bitmap);
-                    in.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }*/
-            //Now the image won't be available. We are going to use Lazy loading in the getView
-            return flowerList;
+        protected String doInBackground(String... params) {
+            String content = HttpManager.getData(params[0]);
+            return content;
         }
 
         @Override
-        protected void onPostExecute(List<Flower> result) {
+        protected void onPostExecute(String result) {
+
             tasks.remove(this);
-            if(tasks.size() == 0) {
-                progressBar.setVisibility(View.INVISIBLE);
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.INVISIBLE);
             }
 
-            //flowerList = FlowerXmlParser.parseFeed(result);
-            if(result == null) {
-                Toast.makeText(MainActivity.this, "Can't connect to webservice", Toast.LENGTH_LONG).show();
-                return;
-            }
-            flowerList = result;
-            updateDisplay();
+            updateDisplay(result);
 
         }
 
-        @Override
-        protected void onProgressUpdate(String... values) {
-            //updateDisplay(values[0]);
-        }
     }
-
 
 }
