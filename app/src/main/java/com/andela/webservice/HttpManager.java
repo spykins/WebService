0e0b6650f71c09f6fa1b68f05740b28f2ff998fb
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-06-20 15:09:39 +0100

    Package the request as the request body to the POST request

--- app/src/main/java/com/andela/webservice/HttpManager.java
+++ app/src/main/java/com/andela/webservice/HttpManager.java
@@ -4,6 +4,7 @@ package com.andela.webservice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -26,8 +27,18 @@ public class HttpManager {
         try {
             URL url = new URL(uri);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
             con.setRequestMethod(p.getMethod()); //This is value, GET, or POST
+            //**** Note*** set the request method before writing the body
+            if(p.getMethod().equals("POST")) {
+                con.setDoOutput(true);
+                // This allows to output content to the body of the request
+                OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+                //creating a write object that can write to the output stream and send information to the connection
+                writer.write(p.getEncodedParams()); // This gets the same param as the Get, with the & and the = sign
+                //all in the same place and the value encoded to send over the web... but instead of appending it to d uri
+                //we will place it in the body of the request
+                writer.flush(); //This flush anything written to the memory
+            }
             StringBuilder sb = new StringBuilder();
             reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
